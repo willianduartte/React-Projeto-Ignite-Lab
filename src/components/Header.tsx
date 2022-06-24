399d7ee47b58c0d92314021cb070c144c981e830
Willian Duarte <willianduartedesouza04@gmail.com>
2022-06-24 19:58:34 -0300

    Projeto finalizado

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import { Logo } from "./Logo"
+
+export const Header = () => {
+  return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+      <Link to="/event">
+        <Logo />
+      </Link>
+    </header>
+  )
+}
